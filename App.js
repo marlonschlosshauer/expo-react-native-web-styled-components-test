14ecdafa8177c4109174dfc50e007ab0b9569e21
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2023-06-30 22:24:53 +0200

    Use styled-components

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { styled } from 'styled-components/native';
+
+const Container = styled.View`
+    flex: 1;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

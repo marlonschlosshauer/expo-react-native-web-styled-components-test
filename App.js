78955f61d94666b7cf5690bb60f03a0cba52a312
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2023-07-02 14:54:37 +0200

    Fix import

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text } from 'react-native';
-import { styled } from 'styled-components/native';
+import styled from 'styled-components/native';
 
 const Container = styled.View`
     flex: 1;
